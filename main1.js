d31c0debf4a955910f8e369b1de39178922a3d01
Andre07G <eleal087@gmail.com>
2025-06-11 09:22:48 -0500

    feat: :sparkles: Added characters
    added spaces to show the characters by the specie
    
    by@Andres

--- main1.js
+++ main1.js
@@ -1,22 +1,50 @@
 //FUNCIONALIDADES JS
 
-//Barras mouseover
 
-const planetasPanel = document.getElementById("aaa")
-planetasPanel.addEventListener("mouseover",planetasMostrar);
-const planetasComplementario = document.getElementById("plane");
-const planeta1 = document.getElementById("planeta1");
-const planeta2 = document.getElementById("planeta2");
-console.log(planetasComplementario.classList)
-function planetasMostrar(){
-    planetasComplementario.classList.replace("planeta-complementario","planeta-complementario-visible");
-   
-}   
-planetasPanel.addEventListener("mouseout",planetasOcultar);
-function planetasOcultar(){
-    planetasComplementario.classList.replace("planeta-complementario-visible","planeta-complementario"); 
+//MAIN PAGE
+//Barras mouseover
+function cargarIndex() {
+    const planetasPanel = document.getElementById("aaa")
+    planetasPanel.addEventListener("mouseover", planetasMostrar);
+    const planetasComplementario = document.getElementById("plane");
+    const planeta1 = document.getElementById("planeta1");
+    const planeta2 = document.getElementById("planeta2");
+    console.log(planetasComplementario.classList)
+    function planetasMostrar() {
+        planetasComplementario.classList.replace("planeta-complementario", "planeta-complementario-visible");
+
+    }
+    planetasPanel.addEventListener("mouseout", planetasOcultar);
+    function planetasOcultar() {
+        planetasComplementario.classList.replace("planeta-complementario-visible", "planeta-complementario");
+    }
+
+    //Enlaces
+
+    //abierto para añadir enlaces principales
 }
 
-//Enlaces
+if (document.body.classList.contains("pagina-index")) {
+    cargarIndex();
+}
+//CHARACTERS
 
-//abierto para añadir enlaces principales
+if (document.body.classList.contains("pagina-personajes")) {
+    cargarPersonajesPage();
+}
+
+function cargarPersonajesPage() {
+    console.log("aaa")
+    const humanos = document.getElementsByClassName("tarhumanos")[0];
+    console.log(humanos)
+    const tarjetaHuman = document.getElementsByClassName("oculto")[0];
+    console.log(tarjetaHuman)
+    humanos.addEventListener("click", mostrarPersonajes);
+    function mostrarPersonajes() {
+        tarjetaHuman.classList.replace("oculto","humanos")
+        humanos.classList.replace("tarhumanos","tarhumanos-extendido")
+    }
+
+
+
+}
