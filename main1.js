753837db8179050dc278fc0b27d2690c7f2f0eaa
Andres07g <eleal087@gmail.com>
2025-06-12 00:14:55 -0500

    feat: :sparkles: Function added
    Function to shows the characters added for every specie
    
    by@Andres

--- main1.js
+++ main1.js
@@ -31,43 +31,75 @@ if (document.body.classList.contains("pagina-index")) {
 
 if (document.body.classList.contains("pagina-personajes")) {
     cargarPersonajesPage();
-}
-
-function cargarPersonajesPage() {
-    const botonHumanos = document.getElementById("boton-mostrar-humanos");
-    const humanos = document.getElementById("tarhumanos")
-    const tarjetaHuman = document.getElementById("personajesHumanos")
-    let humanoClick = false
-    botonHumanos.addEventListener("click",()=>{
-        if(!humanoClick){
-            tarjetaHuman.classList.replace("oculto", "humanos");
-            humanos.classList.replace("tarhumanos", "tarhumanos-extendido");
-            humanoClick = true;
-        }else{
-            tarjetaHuman.classList.replace("humanos","oculto");
-            humanos.classList.replace("tarhumanos-extendido","tarhumanos");
-            humanoClick=false;
-        }
-
-
-    })
-
-
-    // const humanos = document.getElementsByClassName("tarhumanos")[0];
-    // const tarjetaHuman = document.getElementsByClassName("oculto")[0];
-    // console.log(tarjetaHuman)
-    // humanos.addEventListener("click", mostrarPersonajes);
-    
-    // function mostrarPersonajes() {
-    //     
-    // }
-    // const humanosexten = document.getElementById("tarhumano");
-    // console.log(humanosexten)
-    // humanosexten.addEventListener("click",noMostrarPersonajes);
-    // function noMostrarPersonajes(){
-    //     
-    // }
-
-
-
+    cargarPersonajesDroid();
+    cargarPersonajesYoda();
+    cargarPersonajesWookie();
+    function cargarPersonajesPage() {
+        const botonHumanos = document.getElementById("boton-mostrar-humanos");
+        const humanos = document.getElementById("tarhumanos")
+        const tarjetaHuman = document.getElementById("personajesHumanos")
+        let humanoClick = false
+        botonHumanos.addEventListener("click", () => {
+            if (!humanoClick) {
+                tarjetaHuman.classList.replace("oculto", "humanos");
+                humanos.classList.replace("tarhumanos", "tarhumanos-extendido");
+                humanoClick = true;
+            } else {
+                tarjetaHuman.classList.replace("humanos", "oculto");
+                humanos.classList.replace("tarhumanos-extendido", "tarhumanos");
+                humanoClick = false;
+            }
+        })
+    };
+    function cargarPersonajesDroid() {
+        const botonDroid = document.getElementById("boton-mostrar-droid");
+        const droides = document.getElementById("tardroid")
+        const tarjetaDroides = document.getElementById("personajesDroid")
+        let droidClick = false
+        botonDroid.addEventListener("click", () => {
+            if (!droidClick) {
+                tarjetaDroides.classList.replace("oculto", "droid");
+                droides.classList.replace("tardroid", "tardroid-extendido");
+                droidClick = true;
+            } else {
+                tarjetaDroides.classList.replace("droid", "oculto");
+                droides.classList.replace("tardroid-extendido", "tardroid");
+                droidClick = false;
+            }
+        })
+    };
+    function cargarPersonajesYoda() {
+        const botonYoda = document.getElementById("boton-mostrar-yoda");
+        const yodas = document.getElementById("taryoda")
+        const tarjetaYodas = document.getElementById("personajesYoda")
+        let yodaClick = false
+        botonYoda.addEventListener("click", () => {
+            if (!yodaClick) {
+                tarjetaYodas.classList.replace("oculto", "yoda");
+                yodas.classList.replace("taryoda", "taryoda-extendido");
+                yodaClick = true;
+            } else {
+                tarjetaYodas.classList.replace("yoda", "oculto");
+                yodas.classList.replace("taryoda-extendido", "taryoda");
+                yodaClick = false;
+            }
+        })
+    };
+    function cargarPersonajesWookie() {
+        const botonWookie = document.getElementById("boton-mostrar-wookie");
+        const wookies = document.getElementById("tarwookie")
+        const tarjetaWookies = document.getElementById("personajesWookie")
+        let wookieClick = false
+        botonWookie.addEventListener("click", () => {
+            if (!wookieClick) {
+                tarjetaWookies.classList.replace("oculto", "wookie");
+                wookies.classList.replace("tarwookie", "tarwookie-extendido");
+                wookieClick = true;
+            } else {
+                tarjetaWookies.classList.replace("wookie", "oculto");
+                wookies.classList.replace("tarwookie-extendido", "tarwookie");
+                wookieClick = false;
+            }
+        })
+    }
 }
